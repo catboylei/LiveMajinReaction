44dcf4c07c5576ae64af9244356ec7e94e49e434
catboylei <leiwyck@hotmail.com>
2026-06-02 22:07:14 +0200

    init commit

---
+++ src/client/java/majin/live/client/LiveMajinReactionClient.java
@@ -0,0 +1,10 @@
+package majin.live.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LiveMajinReactionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
