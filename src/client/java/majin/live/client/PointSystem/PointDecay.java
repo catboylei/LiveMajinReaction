9827294f54981b69840480a48b94aa055cfbf5fe
catboylei <leiwyck@hotmail.com>
2026-06-03 13:16:21 +0200

    clean up generated config screen

--- src/client/java/majin/live/client/PointSystem/PointDecay.java
+++ src/client/java/majin/live/client/PointSystem/PointDecay.java
@@ -21,14 +21,14 @@ public class PointDecay {
     }
 
     private static void handleDecay() {
-        int current = LiveMajinReactionClient.CONFIG.majinPoints();
+        int current = LiveMajinReactionClient.CONFIG.internalSettings.majinPoints();
         if (current == 0) return;
-        int rate = LiveMajinReactionClient.CONFIG.pointDecay();
+        int rate = LiveMajinReactionClient.CONFIG.internalSettings.pointDecay();
 
         if (current > 0) {
-            LiveMajinReactionClient.CONFIG.majinPoints(Math.max(0, current - rate));
+            LiveMajinReactionClient.CONFIG.internalSettings.majinPoints(Math.max(0, current - rate));
         } else {
-            LiveMajinReactionClient.CONFIG.majinPoints(Math.max(0, current + rate));
+            LiveMajinReactionClient.CONFIG.internalSettings.majinPoints(Math.max(0, current + rate));
         }
     }
 }
--- src/client/java/majin/live/client/config/LiveMajinReactionConfig.java
+++ src/client/java/majin/live/client/config/LiveMajinReactionConfig.java
@@ -2,12 +2,24 @@ package majin.live.client.config;
 
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
+import io.wispforest.owo.config.annotation.Nest;
+import io.wispforest.owo.config.annotation.SectionHeader;
 
 @Modmenu(modId = "live-majin-reaction") // this automatically makes a (bad) config screen from this
 @Config(name = "live-majin-reaction", wrapperName = "LiveMajinReactionConfigGenerated")
 public class LiveMajinReactionConfig {
-
     // add fields to the config here, datagen handles them automatically
-    public int pointDecay = 1;
-    public int majinPoints = 0;
+
+    @SectionHeader("General")
+    public boolean reactionEnabled = true;
+
+    @SectionHeader("Other")
+    @Nest
+    public Internal internalSettings = new Internal();
+
+    public static class Internal {
+        public int pointDecay = 1;
+        public int majinPoints = 0;
+        public boolean debugPrints = false;
+    }
 }
